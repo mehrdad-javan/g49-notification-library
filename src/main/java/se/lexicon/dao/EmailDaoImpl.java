af3d9a272b997190763c65c1295b2e7726552bb6
Mehrdad Javan <mehrdad.javan@lexicon.se>
2024-04-23 09:55:51 +0200

    implement the dao class for email

--- src/main/java/se/lexicon/dao/EmailDaoImpl.java
+++ src/main/java/se/lexicon/dao/EmailDaoImpl.java
@@ -1,37 +1,59 @@
 package se.lexicon.dao;
 
+import se.lexicon.exception.DataNotFoundException;
 import se.lexicon.model.Email;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class EmailDaoImpl implements EmailDao {
 
-    private  List<Email> emailList = new ArrayList<>();
+    private List<Email> emailList = new ArrayList<>();
 
 
     @Override
     public Email save(Email email) {
-        return null;
+        if (email == null) throw new IllegalArgumentException("Email is null.");
+        email.setId(UUID.randomUUID().toString());
+        email.setCreateDateTime(LocalDateTime.now());
+        emailList.add(email);
+        return email;
     }
 
     @Override
     public void update(Email email) {
+        if (email == null) throw new IllegalArgumentException("email is null.");
+        if (email.getId() == null) throw new IllegalArgumentException("email id is null.");
 
+        Email existingEmail = find(email.getId());
+        existingEmail.setRecipient(email.getRecipient());
+        existingEmail.setSubject(email.getSubject());
+        existingEmail.setContent(email.getContent());
     }
 
     @Override
     public Email find(String id) {
-        return null;
+        if (id == null) throw new IllegalArgumentException("Id is null.");
+        return emailList.stream()
+                .filter(email -> email.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new DataNotFoundException("Email data with id:" + id + " not found."));
     }
 
     @Override
     public List<Email> findAll() {
-        return null;
+        return new ArrayList<>(emailList);
     }
 
     @Override
     public List<Email> findBySubject(String subject) {
-        return null;
+        if (subject == null) throw new IllegalArgumentException("Subject is null.");
+        return emailList.stream()
+                .filter(email -> email.getSubject().equalsIgnoreCase(subject))
+                .collect(Collectors.toList());
     }
+
 }
